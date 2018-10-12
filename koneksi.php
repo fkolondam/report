76023f7dbc640167ee7599c6ab1e23a2ef05a683
novaldaud <35998553+novaldaud@users.noreply.github.com>
2018-10-12 09:45:00 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,19 +1,6 @@
   
 <?php
 
-// Server in the this format: <computer>\<instance name> or 
-// <server>,<port> when using a non default port number
-/*
-$server = '172316909513.ip-dynamic.com,1433';
-$connectionInfo = array( "Database"=>"DB_BESTZ");
-// Connect to MSSQL
-$link = sqlsrv_connect($server, 'sa', 'bestpwd');
-
-	// Select a database:
-$pilih_db=mssql_select_db('DB_BEST') 
-   or die('Could not select a database.');
-*/ 
-
 $serverName = 'LENOVO01\BESTSERVER';
 $connectionInfo = array( "Database"=>"DB_BEST12", "UID"=>"sa", "PWD"=>"bestpwd");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
